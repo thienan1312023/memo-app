4c58fdacb287f631bfdedf9e5f1b470dd2666909
annguyenonsolve <an.nguyen@onsolve.com>
2019-08-16 23:04:46 +0700

    Fix authentication

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import { store } from './_helpers';
+import { store } from './helpers';
 import { App } from './App';
 
 // setup fake backend
-import { configureFakeBackend } from './_helpers';
+import { configureFakeBackend } from './helpers';
 configureFakeBackend();
 
 render(
