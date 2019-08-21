84374ca9f36d139fb1c84f30567cef826d1c5ec3
annguyenonsolve <an.nguyen@onsolve.com>
2019-08-21 17:07:19 +0700

    config assets

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const PORT = process.env.PORT || 80;
-//const { mongoose } = require('./db.js');
 
 const app = express();
 
-app.use(express.static('assets'));
+app.use('/assets', express.static('assets'));
 app.use(express.static('dist'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
